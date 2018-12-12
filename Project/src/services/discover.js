46a64e250567f1e8b74e00115b90f28669ce6289
LKYR-git <ws03242018@163.com>
2018-12-12 19:31:29 +0800

    success

--- Project/src/services/discover.js
+++ Project/src/services/discover.js
@@ -1,6 +1,6 @@
 import request from '../utils/request';
 
-const host = /localhost/.test(window.location.host)?'http://123.206.55.50:14000':'';
+const host = /localhost/.test(window.location.host)?'http://123.206.55.50:14000':'http://123.206.55.50:14000';
 
 //搜索歌曲
 export function search(keywords){
--- Project/src/services/index.js
+++ Project/src/services/index.js
@@ -1,7 +1,7 @@
 import request from '../utils/request';
 export * from './discover.js'
 
-const host = /localhost/.test(window.location.host)?'http://123.206.55.50:14000':'';
+const host = /localhost/.test(window.location.host)?'http://123.206.55.50:14000':'http://123.206.55.50:14000';
 
 //登录接口
 export function logins(params){
